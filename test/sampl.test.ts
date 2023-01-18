c2987bd627cf68e28413614dba16cd4984d36f0e
Stefan Ihle <stefanihle@gmx.net>
2023-01-18 11:45:20 +0100

    create test file and a testing function

---
+++ test/sampl.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+import { createUser } from '../script';
+
+test('createUser should return the generated user', async () => {
+	const newUser = { email: 'user@prisma.io', name: 'Prisma Fan' };
+	const user = await createUser(newUser);
+
+	expect(user).toStrictEqual({ ...newUser, id: 1 });
+});
